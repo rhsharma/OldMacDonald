f1be67982d4858abdae0281eaff7674f8aa3a8f9
unknown <rhsharma@697-334-eowyn.sf.k12.internal>
2015-10-29 10:46:50 -0700

    Fixed duplicate code

--- NamedCow.java
+++ NamedCow.java
@@ -1,13 +1,9 @@
 class NamedCow extends Cow
 {
 	private String myName;
-	private String myType;
-	private String mySound;
 	public NamedCow(String type, String cName, String sound)
 	{
 		myName = cName;
-		myType = type;
-		mySound = sound;
 	}
 
 	public String getName()
